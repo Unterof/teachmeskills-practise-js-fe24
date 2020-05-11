fcd26058d4d79242be19d5d964df34bc6b3fb340
Unterof <fisher_box@mail.ru>
2020-05-11 23:00:58 +0300

    last changes on 11.09.2020

--- react_it_kamasutra/react_lesson/src/Components/Dialogs/Dialogs.js
+++ react_it_kamasutra/react_lesson/src/Components/Dialogs/Dialogs.js
@@ -1,9 +1,34 @@
 import React from "react";
 import dialogs from './Dialogs.module.css'
+import {NavLink} from "react-router-dom";
 
 const Dialogs = () => {
     return (
-        <div className={dialogs.container}>Dialogs</div>
+        <div className={dialogs.container}>
+            <div className={dialogs.dialogsItems}>
+                <div className={dialogs.item}>
+                    <NavLink to="/dialogs/1">Kirill</NavLink>
+                </div>
+                <div className={dialogs.item}>
+                    <NavLink to="/dialogs/2"> Dmitry </NavLink>
+                </div>
+                <div className={dialogs.item}>
+                    <NavLink to="/dialogs/3">Ksenya</NavLink>
+                </div>
+                <div className={dialogs.item}>
+                    <NavLink to="/dialogs/4">Valery</NavLink>
+                </div>
+                <div className={dialogs.item}>
+                    <NavLink to="/dialogs/5">Zohan</NavLink>
+                </div>
+            </div>
+            <div className={dialogs.messages}>
+                <div className={dialogs.message}>Hi!</div>
+                <div className={dialogs.message}>Hello how are you?</div>
+                <div className={dialogs.message}>I have enough time</div>
+                <div className={dialogs.message}>You need to learn javascript more!</div>
+            </div>
+        </div>
     )
 }
 
--- react_it_kamasutra/react_lesson/src/Components/Profile.jsx
+++ react_it_kamasutra/react_lesson/src/Components/Profile.jsx
@@ -25,3 +25,5 @@ const Profile = () => {
 };
 
 export default Profile;
+
+
