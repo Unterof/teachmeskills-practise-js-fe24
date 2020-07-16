ce7f526fa43c603123d54e2fbc93476f4f293fc9
Unterof <fisher_box@mail.ru>
2020-07-16 21:20:57 +0300

    add withRotuter

--- react_it_kamasutra/react_lesson/src/Containers/ProfileContainer.js
+++ react_it_kamasutra/react_lesson/src/Containers/ProfileContainer.js
@@ -3,13 +3,18 @@ import axios from "axios";
 import Profile from "../Components/Profile/Profile";
 import {setUserProfile} from "../redux/profile-reducer";
 import {connect} from "react-redux";
+import {withRouter} from "react-router-dom";
 
 
 class ProfileContainer extends React.Component {
 
     componentDidMount() {
 
-        axios.get('https://social-network.samuraijs.com/api/1.0/profile/2').then(response => {
+        let userId = this.props.match.params.userId
+        if (!userId) {
+            userId = 2
+        }
+        axios.get(`https://social-network.samuraijs.com/api/1.0/profile/` + userId).then(response => {
 
             console.log(response.data)
             this.props.setUserProfile(response.data)
@@ -48,7 +53,8 @@ let mapStateToProps = (state) => {
  }
 }
 
-export default connect (mapStateToProps,{setUserProfile})(ProfileContainer)
+let ProfileContainerWithRouter =  withRouter(ProfileContainer)
+export default connect (mapStateToProps,{setUserProfile})(ProfileContainerWithRouter)
 
 
 
